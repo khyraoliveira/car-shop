8dd85d4222b1cf052c886388c1922224c8b3db58
Khyra <khyrak@gmail.com>
2022-10-12 11:43:23 -0300

    req 4 - fix

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -1,8 +1,9 @@
 import { ICar } from '../interfaces/ICar';
+import CarsService from '../services/CarsService';
 import VehicleController from './VehicleController';
 
 class CarsController extends VehicleController<ICar> {
-  constructor(_service = new CarsServices()) {
+  constructor(_service = new CarsService()) {
     super(_service);
   }
 }
---
+++ src/middlewares/error.ts
@@ -0,0 +1,29 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { errorCatalog, ErrorTypes } from './catalog';
+
+const errorHandler: ErrorRequestHandler = ( 
+  err: Error | ZodError, 
+  _req,
+  res,
+  _next,
+) => {
+  if (err instanceof ZodError) { 
+    console.error(err);
+    return res.status(400).json({ message: err.issues });
+  }
+
+  // const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+  const messageAsErrorType = err.message as ErrorTypes;
+
+  const mappedError = errorCatalog[messageAsErrorType];
+  if (mappedError) {
+    const { httpStatus, message } = mappedError;
+    return res.status(httpStatus).json({ error: message });
+  }
+
+  console.error(err);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler; 
