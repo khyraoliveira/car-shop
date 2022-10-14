1afa67599142a3de1fa9adafcb0e9fe26b08cba0
Khyra <khyrak@gmail.com>
2022-10-13 22:05:19 -0300

    motoRcycles - add a bendita rota

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errorsMiddleware from './middlewares/error';
 import carsRouter from './routes/cars';
+import motorcyclesRouter from './routes/motorcycles';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carsRouter);
+app.use('/motorcycles', motorcyclesRouter);
 app.use(errorsMiddleware);
 
 export default app;
