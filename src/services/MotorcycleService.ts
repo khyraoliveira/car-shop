cbb397d020bbbe97af40fcbf5cf280ff437b9f28
Khyra <khyrak@gmail.com>
2022-10-13 21:07:33 -0300

    motoRcycles - fix3

--- src/services/MotorcycleService.ts
+++ src/services/MotorcycleService.ts
@@ -2,10 +2,10 @@ import { IMotorcycle, motorcycleZodSchema } from '../interfaces/IMotorcycle';
 import MotorcyclesModel from '../models/MotorcyclesModel';
 import VehicleService from './VehicleService';
 
-class MotorcyclesService extends VehicleService<IMotorcycle> {
+class MotorcycleService extends VehicleService<IMotorcycle> {
   constructor(protected _model = new MotorcyclesModel(), _zodSchema = motorcycleZodSchema) {
     super(_model, _zodSchema);
   }
 }
 
-export default MotorcyclesService;
+export default MotorcycleService;
